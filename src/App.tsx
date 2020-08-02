bc545eae31e66e1d7bad60f164268faa823731ed
HeavyKen <58620865+HeavyKen@users.noreply.github.com>
2020-08-02 15:55:12 +1000

    added basic drag-drop functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Button } from 'antd';
-import { ClockCircleOutlined, PlusOutlined, DeleteOutlined } from '@ant-design/icons';
-import moment, { MomentZone } from 'moment-timezone'
-import { REPL_MODE_STRICT } from 'repl';
+import { ClockCircleOutlined, DeleteOutlined } from '@ant-design/icons';
 import { TimezonePicker } from "@blueprintjs/timezone";
+import { Button } from 'antd';
+import moment, { MomentZone } from 'moment-timezone';
+import React from 'react';
+import './App.css';
+import { DragDropContext, Droppable, Draggable, DroppableProvided, DraggableProvided, DraggableStateSnapshot, DroppableStateSnapshot, DropResult } from "react-beautiful-dnd";
+
+const reorder = (list: MomentZone[], startIndex: number, endIndex: number) => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  return result;
+};
 
 const App: React.FC = () => {
-
   let [clocks, setClocks] = React.useState([
     moment.tz.zone('Australia/Sydney') as MomentZone,
     moment.tz.zone('America/Vancouver') as MomentZone
@@ -20,10 +26,10 @@ const App: React.FC = () => {
     setIsSettings(!isSettings)
   }
 
-  function handleTimezoneChange(tzName: string){
+  function handleTimezoneChange(tzName: string) {
     setClocks(prev => [...prev, moment.tz.zone(tzName) as MomentZone,])
   }
-ç
+
   let [, setDummy] = React.useState(0)
 
   React.useEffect(() => {
@@ -32,51 +38,98 @@ const App: React.FC = () => {
     }, 500)
   }, [])
 
-  return (
-    <div style={{ display: 'grid', gridTemplateColumns: '10% 15% auto 15% 10%', gridTemplateRows: '25% 15% auto 10%', height: '100vh' }}>
-      <div style={{ display: 'flex', gridColumnStart: 3, gridColumnEnd: 4, gridRowStart: 2, gridRowEnd: 3, justifyContent: 'space-between', alignItems: 'center' }}>
-        <div>
-          <h2>
-            {clocks[0].name} {moment.tz(moment(), clocks[0].name).calendar(moment(), {
-              sameDay: 'h:mm:ssa',
-              nextDay: '[Tomorrow at] h:çmm:ssa',
-              lastDay: '[Yesterday at] h:mm:ssa'
-            })}
-          </h2>
-        </div>
-        <div>
-          <Button type="ghost" icon={<ClockCircleOutlined />}></Button>
-        </div>
-      </div>
-      <div style={{ gridColumnStart: 3, gridColumnEnd: 4, gridRowStart: 3, gridRowEnd: 4 }}>
-        {/* https://dev.to/ibrahima92/15-must-know-javascript-array-methods-in-2020-1kd8 */}
-        {clocks.map((tz, i) => i === 0 ? null : <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-          <div>
-            {tz.name} {moment.tz(moment(), tz.name).calendar(moment(), {
-              sameDay: 'h:mm:ssa',
-              nextDay: '[Tomorrow at] h:çmm:ssa',
-              lastDay: '[Yesterday at] h:mm:ssa'
-            })}
-          </div>
-          <div>
-            <Button type="ghost" icon={<ClockCircleOutlined />}></Button>
-          </div>
-        </div>)}
-        {
-          isSettings ? <>
-            <div style={{ height: 10 }} />
-            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-              <TimezonePicker onChange={handleTimezoneChange} />
-              <Button type="ghost" icon={<DeleteOutlined />}></Button>
-            </div>
-          </> : null
-        }
+  function onDragEnd(result: DropResult) {
+    // dropped outside the list
+    if (!result.destination) {
+      return;
+    }
 
+    const items = reorder(
+      clocks,
+      result.source.index,
+      result.destination.index
+    );
+
+    setClocks(items);
+  }
+
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div style={{ display: 'grid', gridTemplateColumns: '10% 15% auto 15% 10%', gridTemplateRows: '25% 15% auto 10%', height: '100vh' }}>
+        <div style={{ gridColumnStart: 3, gridColumnEnd: 4, gridRowStart: 2, gridRowEnd: 3 }}>
+          <Droppable droppableId="droppableTop">
+            {(provided: DroppableProvided, snapshot: DroppableStateSnapshot) => (
+              <div
+                style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+              >
+                <div>
+                  <h2>
+                    {clocks[0].name} {moment.tz(moment(), clocks[0].name).calendar(moment(), {
+                      sameDay: 'h:mm:ssa',
+                      nextDay: '[Tomorrow at] h:çmm:ssa',
+                      lastDay: '[Yesterday at] h:mm:ssa'
+                    })}
+                  </h2>
+                </div>
+                <div>
+                  <Button type="ghost" icon={<ClockCircleOutlined />}></Button>
+                </div>
+              </div>
+            )}
+          </Droppable>
+        </div>
+        <div style={{ gridColumnStart: 3, gridColumnEnd: 4, gridRowStart: 3, gridRowEnd: 4 }}>
+          <Droppable droppableId="droppable">
+            {(provided: DroppableProvided, snapshot: DroppableStateSnapshot) => (
+              <div
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+              >
+                {/* https://dev.to/ibrahima92/15-must-know-javascript-array-methods-in-2020-1kd8 */}
+                {clocks.map((tz, i) => i === 0 ? null : <Draggable key={i} draggableId={i.toString()} index={i}>
+                  {(provided: DraggableProvided, snapshot: DraggableStateSnapshot) => (
+                    <div
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      style={{ ...provided.draggableProps.style, display: 'flex', justifyContent: 'space-between' }}
+                    >
+                      <div>
+                        {tz.name} {moment.tz(moment(), tz.name).calendar(moment(), {
+                          sameDay: 'h:mm:ssa',
+                          nextDay: '[Tomorrow at] h:çmm:ssa',
+                          lastDay: '[Yesterday at] h:mm:ssa'
+                        })}
+                      </div>
+                      <div>
+                        <Button type="ghost" icon={<ClockCircleOutlined />}></Button>
+                      </div>
+                    </div>
+                  )}
+                </Draggable>
+                )}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+          {
+            isSettings ? <>
+              <div style={{ height: 10 }} />
+              <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                <TimezonePicker onChange={handleTimezoneChange} />
+                <Button type="ghost" icon={<DeleteOutlined />}></Button>
+              </div>
+            </> : null
+          }
+
+        </div>
+        <div style={{ gridColumnStart: 3, gridColumnEnd: 4, gridRowStart: 4, gridRowEnd: 5, placeSelf: 'center' }}>
+          <Button type="ghost" onClick={toggleSettings}>{isSettings ? 'Done' : 'Edit'}</Button>
+        </div>
       </div>
-      <div style={{ gridColumnStart: 3, gridColumnEnd: 4, gridRowStart: 4, gridRowEnd: 5, placeSelf: 'center' }}>
-        <Button type="ghost" onClick={toggleSettings}>{isSettings ? 'Done' : 'Edit'}</Button>
-      </div>
-    </div>
+    </DragDropContext>
   );
 }
 
